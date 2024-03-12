e7c8e83b8a89804aeb2b2fc34e234c045f38c5ac
Joshua Shiman <joshonmail@mail.com>
2024-03-12 13:17:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 # This will handle scheduling and sending Slack messages
+
+# Imports
 import requests
 import json
 
+# WebhookURL secret
 webhookURL = input('What is the webhook URL \n >')
 
+# Read JSON message data
 with open('slackMessage.json') as f:
    slackMessage = json.load(f)
 
-def curl_request(message, url):
-    r = requests.post('url', json= message)
+# curl request function
+def curl_request(message, webhook):
+    r = requests.post(webhook, json= message)
     r.status_code
 
 # Make a curl request to webhookURL
-response = curl_request('slackMessage','webhookURL')
+response = curl_request(slackMessage, webhookURL)
 
 # Make a curl request to webhookURL
 print(response)
